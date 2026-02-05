22a60864a435b3541deb0629d379c31ae8076ef4
UY0063158 <david.spindler@alum.uca.es>
2026-02-05 11:51:04 +0100

    Symon gave data to add, and i started the test_data.py tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,16 @@
-import unittest
+import pytest
+import pandas
+
+from pathlib import Path
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_injury_data_consolidated():
+    my_file = Path("/../data/injury_data_consolidated.xlsx")
+    assert (my_file.exists()), f"File {my_file} does not exist"
+
+def test_injury_data():
+    df = pandas.read_csv("/../data/injury_data_consolidated.xlsx")
+    assert "Ricky Rubio - 2012/13" in df.iloc[:, 0].values, f"Injury data does not contain the tested data"
+
