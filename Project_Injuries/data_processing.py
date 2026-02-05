ba38a808af6500bad33c6e6d498927269cc46f25
Szymo <sk468753@students.mimuw.edu.pl>
2026-02-05 19:12:13 +0100

    cosmetic changes

--- Project_Injuries/data_processing.py
+++ Project_Injuries/data_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def basketball_process_test_train(summary_basketball):
     summary_basketball = summary_basketball.iloc[:, :-2]
@@ -11,7 +12,13 @@ def basketball_process_test_train(summary_basketball):
     basketball_test_after = basketball_after.iloc[20:, :]
     basketball_test_before = basketball_before.iloc[20:, :]
     return (basketball_train_before, basketball_train_after,basketball_test_before, basketball_test_after)
-def merge_vect(x, y):
-    z = np.concatenate((x, y))
 
+def processing_for_lr_plot(train_test_sets):
+
+    train_x, train_y, test_x, test_y = train_test_sets
+    train_x, train_y, test_x, test_y = train_x.loc[:, 'PTS'], train_y.loc[:, 'PTS'], test_x.loc[:, 'PTS'], test_y.loc[:,'PTS']
+    train_x, train_y, test_x, test_y = np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
+    X_all = np.concatenate((train_x, test_x))
+    Y_all = np.concatenate((train_y, test_y))
+    return X_all, Y_all
 
